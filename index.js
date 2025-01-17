2d3fb8a85da8302ddb3f51d51362394107f8d7fe
Abusaleh Alam Khan <abusaleh.uiu.cse@gmail.com>
2025-01-18 00:34:53 +0600

    Implement Moderator dashboard to fetch data

--- index.js
+++ index.js
@@ -42,6 +42,28 @@ async function run() {
       res.send("TechElevate Server is Running!");
     });
 
+    // Fetch user by email
+    app.get("/users", async (req, res) => {
+      const email = req.query.email;
+
+      if (!email) {
+        return res.status(400).json({ message: "Email is required" });
+      }
+
+      try {
+        const user = await usersCollection.findOne({ email });
+
+        if (user) {
+          res.status(200).json(user);
+        } else {
+          res.status(404).json({ message: "User not found" });
+        }
+      } catch (error) {
+        console.error("Error fetching user:", error.message);
+        res.status(500).json({ message: "Internal server error" });
+      }
+    });
+
     // Get products for a specific user
     app.get("/products", async (req, res) => {
       const ownerEmail = req.query.ownerEmail;
